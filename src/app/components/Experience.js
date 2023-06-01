ee12177477efcca407161843b20883f8a4bc0ecf
Duncan Krey <duncankrey@Duncans-MacBook-Air.local>
2023-06-01 15:48:09 -0700

    Added hover effects to experiences

--- src/app/components/Experience.js
+++ src/app/components/Experience.js
@@ -23,9 +23,10 @@ export default function Experience() {
 
     return (
         <li className="mb-12">
-            <div className="group relative grid grid-cols-8 gap-4">
-                <header className="col-span-2 text-xs mt-1 font-semibold uppercase text-slate-500">2018 - Present</header>
-                <div className="col-span-6">
+            <div className="group relative grid grid-cols-8 gap-4 pb-1 transition-all lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <header className="col-span-2 text-xs mt-1 font-semibold uppercase text-slate-500 z-10">2018 - Present</header>
+                <div className="col-span-6 z-10">
                     <h3 className="text-medium leading-snug text-slate-200">
                         <div className="font-medium hover:text-blue-300 focus-visible:text-blue-300">Lead Engineer · Upstatement</div>
                     </h3>
