fcdd0868e709c722aa84fe3dcbdaaf656579ad77
Duncan Krey <duncankrey@Duncans-MacBook-Air.local>
2023-06-01 16:41:24 -0700

    Added animation to name and updated logo and site title

--- src/app/components/Experience.js
+++ src/app/components/Experience.js
@@ -2,11 +2,11 @@ export default function Experience() {
     const technologies = [
         "React",
         "React Native",
-        "SCSS",
-        "WordPress",
         "JavaScript",
-        "TypeScript",
-        "PHP"
+        "HTML",
+        "CSS",
+        "SQL",
+        "Node.js"
     ]
     
     function renderTechnologies() {
@@ -25,13 +25,13 @@ export default function Experience() {
         <li className="mb-12">
             <div className="group relative grid grid-cols-8 gap-4 pb-1 transition-all lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                 <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                <header className="col-span-2 text-xs mt-1 font-semibold uppercase text-slate-500 z-10">2018 - Present</header>
+                <header className="col-span-2 text-xs mt-1 font-semibold uppercase text-slate-500 z-10">2022 - Present</header>
                 <div className="col-span-6 z-10">
                     <h3 className="text-medium leading-snug text-slate-200">
-                        <div className="font-medium hover:text-blue-300 focus-visible:text-blue-300">Lead Engineer · Upstatement</div>
+                        <div className="font-medium hover:text-blue-300 focus-visible:text-blue-300">Frontend Developer · Nexus UW</div>
                     </h3>
                     <p className="mt-2 text-sm leading-normal text-slate-400">
-                        Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.
+                        Programming dynamic React components for a student hub, facilitating local project discovery. Implementing user feedback to enhance style and functionality, optimizing user experience. Collaborating with the design team to bring the website to life through a fusion of design and functionality. Ensuring smooth frontend-to-database connection by working closely with the backend team.
                     </p>
                     <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
                         {renderTechnologies()}
--- src/app/practice/page.js
+++ src/app/practice/page.js
@@ -1,11 +1,33 @@
+'use client';
+import SplitType from "split-type"
 import Experience from "../components/Experience"
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
 
 export default function Home() {
+  useEffect(() => {
+    let text = SplitType.create('.name-text');
+    let characters = document.querySelectorAll('.char');
+
+    for(let i=0; i<characters.length; i++) {
+      characters[i].classList.add('translate-y-full');
+    }
+
+    document.querySelector('.name-text').classList.remove('invisible');
+
+    gsap.to('.char', {
+      y: 0,
+      stagger: 0.05,
+      delay: 0.02,
+      duration: 0.5
+    });
+  }, [])
+
   return (
     <div className='mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24'>
       <div className='lg:flex lg:justify-between lg:gap-4'>
         <header className='lg:py-24'>
-          <h1 className='text-4xl font-bold'>Duncan Krey</h1>
+          <h1 className='name-text text-4xl font-bold invisible' style={{'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'}}>Duncan Krey</h1>
           <h2 className='mt-3 text-lg font-medium'>Frontend Developer</h2>
           <p className='max-w-xs mt-4'>I am passionate about web experiences that empower the user.</p>
         </header>
