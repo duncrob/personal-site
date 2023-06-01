6d2ab09d086662d8886d8550019b70be3872cf5e
Duncan Krey <duncankrey@Duncans-MacBook-Air.local>
2023-06-01 14:14:41 -0700

    Finished about layout, began creating experience component

---
+++ components/Experience.js
@@ -0,0 +1,5 @@
+export default function Experience() {
+    return (
+        <div>This is an experience</div>
+    )
+}
--- src/app/practice/page.js
+++ src/app/practice/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Experience from "../../../components/Experience"
 
 export default function Home() {
   return (
@@ -9,9 +9,22 @@ export default function Home() {
           <h2 className='mt-3 text-lg font-medium'>Frontend Developer</h2>
           <p className='max-w-xs mt-4'>I am passionate about web experiences that empower the user.</p>
         </header>
-        <div className='lg:py-24'>
-          This is column 2
-        </div>
+        <main id='content' className='lg:py-24 lg:w-1/2 '>
+          <section id='about' className='lg:mb-36 lg:scroll-mt-24' aria-label='About me'>
+            <p className='mb-4'>
+              Hello! I'm Duncan, a senior at the University of Washington. I am currently in the Informatics program as an aspiring front-end developer. I am passionate about design that empowers the user.
+            </p>
+            <p className='mb-4'>
+              My recent experience has been working on websites for various organizations at UW. In my off time, I love enjoying coffee and going snowboarding.
+            </p>
+          </section>
+          <section id='experience'>
+            <ol>
+              <Experience />
+              <Experience />
+            </ol>
+          </section>
+        </main>
       </div>
     </div>
   )
