b36dfb07497a78103a132b6db1a7d1814f11a5da
duncrob <duncrob@uw.edu>
2023-06-05 22:24:44 -0700

    Made only right side scroll, added functioning nav bar to left side.

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + 'bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-blue-300 selection:text-blue-900'}>{children}</body>
     </html>
   )
 }
--- src/app/practice/page.js
+++ src/app/practice/page.js
@@ -16,7 +16,7 @@ export default function Home() {
     document.querySelector('.name-text').classList.remove('invisible');
 
     gsap.to('.char', {
-      y: 0,
+      y: -3,
       stagger: 0.05,
       delay: 0.02,
       duration: 0.5
@@ -24,29 +24,58 @@ export default function Home() {
   }, [])
 
   return (
-    <div className='mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24'>
-      <div className='lg:flex lg:justify-between lg:gap-4'>
-        <header className='lg:py-24'>
-          <h1 className='name-text text-4xl font-bold invisible' style={{'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'}}>Duncan Krey</h1>
-          <h2 className='mt-3 text-lg font-medium'>Frontend Developer</h2>
-          <p className='max-w-xs mt-4'>I am passionate about web experiences that empower the user.</p>
-        </header>
-        <main id='content' className='lg:py-24 lg:w-1/2 '>
-          <section id='about' className='lg:mb-36 lg:scroll-mt-24' aria-label='About me'>
-            <p className='mb-4'>
-              Hello! I'm Duncan, a senior at the University of Washington. I am currently in the Informatics program as an aspiring front-end developer. I am passionate about design that empowers the user.
-            </p>
-            <p className='mb-4'>
-              My recent experience has been working on websites for various organizations at UW. In my off time, I love enjoying coffee and going snowboarding.
-            </p>
-          </section>
-          <section id='experience'>
-            <ol className="group/list">
-              <Experience />
-              <Experience />
-            </ol>
-          </section>
-        </main>
+    <div className='relative'>
+      <div className='mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24'>
+        <div className='lg:flex lg:justify-between lg:gap-4'>
+          <header className='lg:py-24 lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between max-h-screen'>
+            <div>
+              <h1 className='name-text text-4xl font-bold invisible' style={{'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'}}>Duncan Krey</h1>
+              <h2 className='mt-3 text-lg font-medium'>Frontend Developer</h2>
+              <p className='max-w-xs mt-4'>I am passionate about web experiences that empower the user.</p>
+              <nav className='nav lg:block' aria-label='In-page jump links'>
+                <ul className='mt-16 w-max'>
+                  <li>
+                    <a className='group flex items-center py-3' href='#about'>
+                      <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                      <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>About</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a className='group flex items-center py-3' href='#experience'>
+                      <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                      <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>Experience</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a className='group flex items-center py-3' href='#projects'>
+                      <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                      <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>Projects</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <ul className='ml-1 mt-8 flex items-center' aria-label='Social Media'>
+            <li>Link</li>
+            </ul>
+          </header>
+          <main id='content' className='lg:py-24 lg:w-1/2'>
+            <section id='about' className='lg:mb-36 lg:scroll-mt-24' aria-label='About me'>
+              <p className='mb-4'>
+                Hello! I&apos;m Duncan, a senior at the University of Washington. I am currently in the Informatics program as an aspiring front-end developer. I am passionate about design that empowers the user.
+              </p>
+              <p className='mb-4'>
+                My recent experience has been working on websites for various organizations at UW. In my off time, I love enjoying coffee and going snowboarding.
+              </p>
+            </section>
+            <section id='experience'>
+              <ol className="group/list">
+                <Experience />
+                <Experience />
+              </ol>
+            </section>
+          </main>
+        </div>
       </div>
     </div>
   )
