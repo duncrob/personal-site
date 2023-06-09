cfef840437e947e3f93fb063e4ea552c53d07f1c
Duncan Krey <duncankrey@Duncans-MacBook-Air.local>
2023-06-09 16:34:14 -0700

    Began to reformat experiences, adding in animated arrow

--- src/app/components/Experience.js
+++ src/app/components/Experience.js
@@ -1,3 +1,5 @@
+import {FiArrowUpRight} from "react-icons/fi";
+
 export default function Experience({ experience }) {
     const technologies = [
         "React",
@@ -28,7 +30,17 @@ export default function Experience({ experience }) {
                 <header className="col-span-2 text-xs mt-1 font-semibold uppercase text-slate-500 z-10">{experience.start} - {experience.end}</header>
                 <div className="col-span-6 z-10">
                     <h3 className="text-medium leading-snug text-slate-200">
-                        <div className="font-medium hover:text-blue-300 focus-visible:text-blue-300">{experience.position} · {experience.company}</div>
+                        <div>
+                            <a href="" target="_blank" rel="noreferrer" aria-label={experience.position + " at " + experience.company} className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-blue-300 focus-visible:text-blue-300 group/link text-base">
+                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                                <span>
+                                    {experience.position + " · "}
+                                    <span className="inline-block">
+                                        {experience.company}<FiArrowUpRight />
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
                     </h3>
                     <p className="mt-2 text-sm leading-normal text-slate-400">{experience.desc}</p>
                     <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
--- src/app/practice/page.js
+++ src/app/practice/page.js
@@ -87,10 +87,15 @@ export default function Home() {
                     <span className="border-b border-transparent pb-px transition group-hover:border-blue-300 motion-reduce:transition-none">View Full </span>
                     <span className="whitespace-nowrap" >
                       <span className="border-b border-transparent pb-px transition group-hover:border-blue-300 motion-reduce:transition-none" >Résumé</span>
-                      <FontAwesomeIcon className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" icon={faArrowRight} />
+                      <FontAwesomeIcon aria-hidden className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" icon={faArrowRight} />
                     </span>
                   </span>
                 </a>
+              </div>
+            </section>
+            <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Selected projects">
+              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Projects</h2>
               </div>
             </section>
           </main>
