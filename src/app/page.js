bed72546def71f0cb049e2051f230423ac196ace
duncrob <duncrob@uw.edu>
2023-06-13 23:13:24 -0700

    Added uw effects and cute snowboarder cursor

--- src/app/page.js
+++ src/app/page.js
@@ -110,10 +110,41 @@ export default function Home() {
               </div>
               <div>
                 <p className='mb-4'>
-                  Hello! I&apos;m Duncan, a recent graduate from the <a className="font-medium text-slate-200 hover:text-blue-300 focus-visible:text-blue-300" href="https://www.washington.edu/" target="_blank" rel="noreferrer">University of Washington</a>. I graduated with a Bachelor of Science in <a className="font-medium text-slate-200 hover:text-blue-300 focus-visible:text-blue-300" href="https://ischool.uw.edu/programs/informatics/what-is-informatics" target="_blank" rel="noreferrer">Informatics</a>, leveraging my skills to become a frontend developer.
+                  Hello! I&apos;m a recent graduate from the&nbsp;
+                  <a className="font-medium text-slate-200" href="https://www.washington.edu/" target="_blank" rel="noreferrer">
+                    <span className="group/uw inline-flex">
+                      <span className="sr-only">University of Washington</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[50ms]">U</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[75ms]">n</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[100ms]">i</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[125ms]">v</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[150ms]">e</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[175ms]">r</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[200ms]">s</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[225ms]">i</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[250ms]">t</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[275ms]">y</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[300ms]">&nbsp;</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[325ms]">o</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[350ms]">f</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[375ms]">&nbsp;</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[400ms]">W</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[425ms]">a</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[450ms]">s</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[475ms]">h</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[500ms]">i</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[525ms]">n</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[550ms]">g</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[575ms]">t</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[600ms]">o</span>
+                      <span className="group-hover/uw:text-purple-400 transition duration-75 group-hover/uw:-translate-y-px delay-[625ms]">n</span>
+                    </span>
+                  </a>
+                  . I graduated with a Bachelor of Science in <a className="font-medium text-slate-200 hover:text-blue-300 focus-visible:text-blue-300" href="https://ischool.uw.edu/programs/informatics/what-is-informatics" target="_blank" rel="noreferrer">Informatics</a>, leveraging my skills to become a frontend developer.
                 </p>
                 <p className='mb-4'>
-                  My recent experience has been working on websites for various organizations at UW. In my off time, I love enjoying coffee and going snowboarding.
+                  My recent experience has been working on websites for various organizations at UW. In my off time, I love enjoying coffee and going&nbsp;
+                  <span className={"lg:cursor-[url('/snowboarder.png'),_pointer]"}>snowboarding</span>.
                 </p>
               </div>
             </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'uwPurple': '#4b2e83',
+        'uwGold': '#e8e3d3'
+      }
     },
   },
   plugins: [],
