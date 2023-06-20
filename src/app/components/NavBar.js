b2cf26b31d573dc79ecc17925d1f2c037e7c23e9
duncrob <duncrob@uw.edu>
2023-06-19 21:48:53 -0700

    fixed nav links for now to focus on restructuring first

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,12 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useScrollPosition } from "../hooks/useScrollPosition"
 
 export default function NavBar() {
+    const baseNavClasses = "nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none";
+    let [aboutClasses, setAboutClasses] = useState(baseNavClasses);
+    let [experienceClasses, setExperienceClasses] = useState(baseNavClasses);
+    let [projectClasses, setProjectClasses] = useState(baseNavClasses);
+
+    let [experienceYPos, setExperienceYPos] = useState(0);
+
     let experiencePosY = 0, projectsPosY
-    
+
     useEffect(() => {
-        experiencePosY = document.getElementById('experience').offsetTop;
-        console.log(experiencePosY);
+        setExperienceYPos(document.getElementById('experience').offsetTop);
+        console.log(experienceYPos);
+
+        setAboutClasses(
+            classNames(
+                scrollPosition > experiencePosY ? 'w-16 bg-slate-200' : 'group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200',
+                'nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all motion-reduce:transition-none'
+            )
+        )
     }, [])
 
     const scrollPosition = useScrollPosition();
@@ -20,50 +34,20 @@ export default function NavBar() {
             <ul className='mt-16 w-max'>
                 <li>
                     <a className='group flex items-center py-3' href='#about'>
-                        <span 
-                            className={classNames(
-                                scrollPosition < experiencePosY ? 'w-16 bg-slate-200' : 'group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200',
-                                'nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all motion-reduce:transition-none'
-                            )}
-                        ></span>
-                        <span 
-                            className={classNames(
-                                scrollPosition < experiencePosY ? 'text-slate-200' : 'text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200',
-                                'nav-text text-xs font-bold uppercase tracking-widest'
-                            )}
-                        >About</span>
+                        <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                        <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>About</span>
                     </a>
                 </li>
                 <li>
                     <a className='group flex items-center py-3' href='#experience'>
-                        <span className={
-                            classNames(
-                                scrollPosition >= experiencePosY ? 'w-16 bg-slate-200' : 'group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200',
-                                'nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all motion-reduce:transition-none'
-                            )}
-                        ></span>
-                        <span 
-                            className={classNames(
-                                scrollPosition >= experiencePosY ? 'text-slate-200' : 'text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200',
-                                'nav-text text-xs font-bold uppercase tracking-widest'
-                            )}
-                        >Experience</span>
+                        <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                        <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>Experience</span>
                     </a>
                 </li>
                 <li>
                     <a className='group flex items-center py-3' href='#projects'>
-                        <span className={
-                            classNames(
-                                scrollPosition < 410 ? 'w-16 bg-slate-200' : 'group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200',
-                                'nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all motion-reduce:transition-none'
-                            )}
-                        ></span>
-                        <span 
-                            className={classNames(
-                                scrollPosition < 410 ? 'text-slate-200' : 'text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200',
-                                'nav-text text-xs font-bold uppercase tracking-widest'
-                            )}
-                        >Projects</span>
+                        <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                        <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>Projects</span>
                     </a>
                 </li>
             </ul>
